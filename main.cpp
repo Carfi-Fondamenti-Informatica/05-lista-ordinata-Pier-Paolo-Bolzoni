d89a18f9e6da92d37a9731fd3b7bfdfb7ab59aaf
Pier-Paolo-Bolzoni <100352364+Pier-Paolo-Bolzoni@users.noreply.github.com>
2023-05-03 16:35:11 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,19 +8,14 @@ int main() {
     int a=0;
     cin >> a;
 
-
-    int x=9999999;
     for(int i=0;i<10;i++){
-        if(a<lista[i])
-            x=i;
-        break;
-    }
-    if(x!=9999999){
-        for(int j=9;j>x;j--){
-            lista[j] = lista[j-1];
+        if(a<lista[i]){
+            for(int j=9;j>i;j--){
+               lista[j]= lista[j-1];
+            }
+            lista[i]=a;
+            break;
         }
-        lista [x] =a;
-        break;
     }
 
     for(int i=0;i<10;i++){
